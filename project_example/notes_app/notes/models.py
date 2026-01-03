c518526d2ec96acae1016059a23a7e3c4602a530
unknown <olenkabeka@gmail.com>
2026-01-03 14:05:40 +0200

    Create homework folder

--- project_example/notes_app/notes/models.py
+++ project_example/notes_app/notes/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.title
